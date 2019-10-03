01ebead5095bd3c42397c3f29c03ffa4e48cdf7a
Vian <howrain1@gmail.com>
2019-10-03 12:19:33 +0700

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TulisanBerjalan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKeluar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblAnimasi.Left -= 10;
+            if (lblAnimasi.Left < -lblAnimasi.Width)
+            {
+                lblAnimasi.Left = this.Width;
+            }
+        }
+    }
+}
